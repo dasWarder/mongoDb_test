b965b14e2c87c1880d51723286ee36007443f870
dasWarder <Al.94_94@mail.ru>
2021-05-08 10:32:47 +0300

    Add swagger description to model

--- src/main/java/com/example/mongodb_demo/model/Issue.java
+++ src/main/java/com/example/mongodb_demo/model/Issue.java
@@ -1,11 +1,16 @@
 package com.example.mongodb_demo.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -14,9 +19,25 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Issue {
 
     @Id
+    @Schema(description = "unique identifier of the issue",
+            example = "fasf344f4f3h", required = true)
     private String id;
+
+    @Schema(description = "description of the issue",
+            example = "Fix the bug", required = true)
+    @NotBlank
+    @Size(max = 250)
     private String description;
+
+    @Schema(description = "severity of the issue",
+            example = "4", required = true)
     private int severity;
+
+    @Schema(description = "Assignee for working out the solution",
+            example = "Jack", required = true)
+    @NotBlank
+    @NotEmpty
+    @Size(max = 100)
     private String assignee;
 
     public Issue(String description, int severity, String assignee) {
